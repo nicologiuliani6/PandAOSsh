fa43626e5a4f33701ab0722c2b6f8d47ae4e893a
gaialongoo <gaia.longo3@studio.unibo.it>
2025-12-04 12:58:12 +0100

    ora vanno :) merito mio (le prime 3 funzioni)

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -4,39 +4,55 @@ static struct list_head pcbFree_h;
 static pcb_t pcbFree_table[MAXPROC];
 static int next_pid = 1;
 
+/* Initialize "pcbFree_h" and add elements of "pcbFree_table" to the list "pcbFree_h"*/
 void initPcbs() {
-    INIT_LIST_HEAD(&pcbFree_h);
+    int i;
+    INIT_LIST_HEAD(&pcbFree_h);   // inizializza la lista come vuota
 
-    for (int i = 0; i < MAXPROC; i++) {
-        freePcb(&pcbFree_table[i]);     // reset completo
-        pcbFree_table[i].p_pid = next_pid++;
-        list_add_tail(&pcbFree_table[i].p_list, &pcbFree_h);
+    for (i = 0; i < MAXPROC; i++) {
+        INIT_LIST_HEAD(&pcbFree_table[i].p_list);   // inizializza il campo p_list
+        pcbFree_table[i].p_pid = 0;                 // reset PID
+        list_add_tail(&pcbFree_table[i].p_list, &pcbFree_h); // aggiungi alla lista dei PCB liberi
     }
 }
 
-
+/* Add PCB "p" to the list "pcbFree_h" */
 void freePcb(pcb_t* p) {
-    p->p_parent = NULL;
-    INIT_LIST_HEAD(&p->p_child);
-    INIT_LIST_HEAD(&p->p_sib);
-    p->p_semAdd = NULL;
-    p->p_supportStruct = NULL;
-    p->p_prio = 0;
-    p->p_time = 0;
-    p->p_s.entry_hi = 0;
-    p->p_s.cause = 0;
-    p->p_s.status = 0;
-    p->p_s.pc_epc = 0;
-    p->p_s.mie = 0;
-    for (int i = 0; i < STATE_GPR_LEN; i++) {
-        p->p_s.gpr[i] = 0;
+    if (p != NULL) {
+        list_add_tail(&p->p_list, &pcbFree_h); // reinserisci in coda alla lista dei liberi
     }
-    p->p_pid = 0;
 }
 
+/* Allocate new PCB removing one from list "pcbFree_h" if possible */
 pcb_t* allocPcb() {
-    
+    struct list_head* pos;
+    pcb_t* new_pcb;
+
+    if (list_empty(&pcbFree_h)) {
+        return NULL;  // nessun PCB disponibile
+    }
+
+    // prendi il primo elemento dalla lista
+    pos = pcbFree_h.next;
+    list_del(pos);
+
+    // ottieni il PCB corrispondente usando container_of
+    new_pcb = container_of(pos, pcb_t, p_list);
+
+    // inizializza i campi del PCB
+    new_pcb->p_pid = next_pid++;
+    INIT_LIST_HEAD(&new_pcb->p_list);   // resetta i puntatori della lista
+    INIT_LIST_HEAD(&new_pcb->p_child);  // inizializza lista figli
+    INIT_LIST_HEAD(&new_pcb->p_sib);    // inizializza lista fratelli
+    new_pcb->p_parent = NULL;
+    new_pcb->p_semAdd = NULL;
+    new_pcb->p_supportStruct = NULL;
+    new_pcb->p_time = 0;
+    new_pcb->p_prio = 0;
+
+    return new_pcb;
 }
+
 
 void mkEmptyProcQ(struct list_head* head) {
 }
