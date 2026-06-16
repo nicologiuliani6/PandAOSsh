c75763d3bcc497b213ebcd2f6ef5fa6ae859af8b
nicologiuliani6 <nicolo.giuliani6@studio.unibo.it>
2026-06-16 14:16:08 +0200

    doc

--- build/CMakeFiles/MultiPandOS_phase2.dir/klog.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/MultiPandOS_phase2.dir/klog.c.o: \
- /home/nico/Desktop/PandAOSsh/klog.c
--- build/CMakeFiles/MultiPandOS_phase2.dir/link.d
+++
@@ -1,4 +0,0 @@
-MultiPandOS_phase2: \
-  /usr/local/share/uriscv/uriscvcore.ldscript
-
-/usr/local/share/uriscv/uriscvcore.ldscript:
